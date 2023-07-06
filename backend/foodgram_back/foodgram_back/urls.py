28102225801644b686d5e9ca870323f473f20135
ubuntuUser <vsevolodrybnik@gmail.com>
2023-07-06 13:54:27 +0300

    review

--- backend/foodgram_back/foodgram_back/urls.py
+++ backend/foodgram_back/foodgram_back/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/', include('djoser.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
